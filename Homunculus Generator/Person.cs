f8eb8fbce7491d69e0ad95fe87c6b1dab75eb0db
SoapyTarantula <soapytarantula@gmail.com>
2022-09-25 14:38:54 -0500

    Initial upload of the main files for the generator
    Still trying to split the arrays into their own libraries.

---
+++ Homunculus Generator/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleTestApp
+{
+    public class Person
+    {
+        readonly static Random rand = new Random();
+        public int arms, legs, eyes, hands, feet = 0;
+        public float fingers, toes = 0;
+        public string name, diet;
+        public string[] dietArray = { "soup.", "toast.", "candy.", "nectarines.", "fish.", "apples.", "beef.", "various meats.", "inedible substances.", "free samples at the department store.", "plants.", "sunshine.", "ramen.", "pho.", "sushi.", "pizza rolls.", "spagheti." };
+        public string[] nameArray = {"Mokiro Kuzen", "Laced Azalea", "Nia Kohana", "Isabella Esperia", "Ophelia Vexx", "Jessie Zaeora", "Rosaria Shinozaki", "Sai Hamori", "Macaroni Bowl", "Doctor Dubu", "Rustie Yiffany", "Danny Naoko", "Cheap Shot", "Tea Oolong", "Flash Reaper", "Kait Oreal"};
+
+        public Person() 
+        {
+            int indexOfNames = rand.Next(nameArray.Length);
+            this.name = nameArray[indexOfNames];
+            GiveMeBodyParts(this);
+        }
+
+        public Person(string whatIsMyName)
+        {
+            this.name = whatIsMyName;
+            GiveMeBodyParts(this);
+        }
+
+        private void GiveMeBodyParts(Person _person)
+        {
+            int indexOfDiet = rand.Next(dietArray.Length);
+            _person.diet = dietArray[indexOfDiet];
+            _person.arms = rand.Next(0, 4);
+            _person.legs = rand.Next(0, 4);
+            _person.eyes = rand.Next(0, 4);
+            _person.hands = rand.Next(0, 4);
+            _person.feet = rand.Next(0, 4);
+            _person.fingers = rand.Next(0, 16);
+            _person.toes = rand.Next(0, 20);
+        }
+    }
+}
---
+++ Homunculus Generator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleTestApp
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            var duder = new Person();
+            var otherDude = new Person();
+            PersonPrinter(duder);
+            Console.WriteLine("");
+            PersonPrinter(otherDude);
+            Console.ReadKey(true);
+        }
+
+        public static void PersonPrinter(Person p1)
+        {
+            // name
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(p1.name);
+            Console.ResetColor(); // Resets the color of the text to the default.
+            Console.WriteLine(""); // Add some whitespace.
+            // diet
+            Console.WriteLine(p1.name + " consumes mostly " + p1.diet);
+            // eyes
+            if (p1.eyes == 0) { Console.WriteLine(p1.name + " has no eyes."); }
+            else if (p1.eyes == 1) { Console.WriteLine(p1.name + " has 1 eye."); }
+            else { Console.WriteLine(p1.name + " has " + p1.eyes + " eyes."); }
+            // arms
+            if (p1.arms == 0) { Console.WriteLine(p1.name + " has no arms."); }
+            else if (p1.arms == 1) { Console.WriteLine(p1.name + " has 1 arm."); }
+            else { Console.WriteLine(p1.name + " has " + p1.arms + " arms."); }
+            // hands
+            if (p1.hands == 0) { Console.WriteLine(p1.name + " has no hands."); }
+            else if (p1.hands == 1) { Console.WriteLine(p1.name + " has 1 hand."); }
+            else { Console.WriteLine(p1.name + " has " + p1.hands + " hands."); }
+            // fingers
+            if (p1.fingers == 0) { Console.WriteLine(p1.name + " has no fingers."); }
+            else if (p1.fingers == 1) { Console.WriteLine(p1.name + " has 1 finger."); }
+            else { Console.WriteLine(p1.name + " has " + p1.fingers + " fingers."); }
+            // legs
+            if (p1.legs == 0) { Console.WriteLine(p1.name + " has no legs."); }
+            else if (p1.legs == 1) { Console.WriteLine(p1.name + " has 1 leg."); }
+            else { Console.WriteLine(p1.name + " has " + p1.legs + " legs."); }
+            // feet
+            if (p1.feet == 0) { Console.WriteLine(p1.name + " has no feet."); }
+            else if (p1.feet == 1) { Console.WriteLine(p1.name + " has 1 foot."); }
+            else { Console.WriteLine(p1.name + " has " + p1.feet + " feet."); }
+            // toes
+            if (p1.toes == 0) { Console.WriteLine(p1.name + " has no toes."); }
+            else if (p1.toes == 1) { Console.WriteLine(p1.name + " has 1 toes."); }
+            else { Console.WriteLine(p1.name + " has " + p1.toes + " toes."); }
+        }
+    }
+}
