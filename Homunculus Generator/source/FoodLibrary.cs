91f3d1e37ebe7517ac1fe9dc80fd3edbde727d72
SoapyTarantula <soapytarantula@gmail.com>
2022-09-25 16:37:36 -0500

    Some sorting and reworking

---
+++ Homunculus Generator/source/FoodLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FoodLibrary
+{
+    public class Food
+    {
+        readonly private static Random randomNum = new Random();
+        public static String GetFood() 
+        {
+            string[] dietArray = { "chicken noodle soup.", "toast.", "candy.", "existential dread.", "nectarines.", "fish.", "apples.", "beef.", "french onion soup.", "various meats.", "inedible substances.", "free samples at the department store.", "plants.", "sunshine.", "ramen.", "pho.", "sushi.", "pizza rolls.", "spaghetti.", "Lalafell flesh.", "dirt.", "a cool rock they found." };
+            var i = randomNum.Next(0, dietArray.Length);
+            return dietArray[i];
+         }
+    }
+}
---
+++ Homunculus Generator/source/People.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PeopleLibrary
+{
+    public class People
+    {
+        readonly private static Random randomNum = new Random();
+        public static String GetPerson() 
+        {
+            string[] nameArray = { "Mokiro Kuzen", "Laced Azalea", "Nia Kohana", "Isabella Esperia", "Ophelia Vexx", "Jessie Zaeora", "Rosaria Shinozaki", "Sai Hamori", "Macaroni Bowl", "Doctor Dubu", "Rustie Yiffany", "Danny Naoko", "Cheap Shot", "Tea Oolong", "Flash Reaper", "Kait Oreal", "Nellie Antoinette", "Seraph Bell", "Zen Rancor", "Myras Gunhilda", "Slumpy Wing" };
+            var i = randomNum.Next(0, nameArray.Length);
+            return nameArray[i];
+        }
+    }
+}
+
