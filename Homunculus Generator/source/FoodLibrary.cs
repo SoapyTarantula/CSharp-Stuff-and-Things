4e00deea497628bdada036914ef4664a382c751e
SoapyTarantula <soapytarantula@gmail.com>
2022-09-25 19:16:44 -0500

    "optimization"
    Honestly I've got no idea if this is better or not.

--- Homunculus Generator/source/FoodLibrary.cs
+++ Homunculus Generator/source/FoodLibrary.cs
@@ -4,11 +4,10 @@ namespace FoodLibrary
 {
     public class Food
     {
-        readonly private static Random randomNum = new Random();
         public static String GetFood() 
         {
-            string[] dietArray = { "chicken noodle soup.", "toast.", "candy.", "existential dread.", "nectarines.", "fish.", "apples.", "beef.", "french onion soup.", "various meats.", "inedible substances.", "free samples at the department store.", "plants.", "sunshine.", "ramen.", "pho.", "sushi.", "pizza rolls.", "spaghetti.", "Lalafell flesh.", "dirt.", "a cool rock they found." };
-            var i = randomNum.Next(0, dietArray.Length);
+            string[] dietArray = { "chicken noodle soup.", "toast.", "candy.", "existential dread.", "nectarines.", "fish.", "apples.", "beef.", "french onion soup.", "various meats.", "inedible substances.", "free samples at the department store.", "plants.", "sunshine.", "ramen.", "pho.", "sushi.", "pizza rolls.", "spaghetti.", "Lalafell flesh.", "dirt.", "a cool rock they found.", "nothing but air.", "the lifeforce of others around them.", "macaroni." };
+            var i = new Random().Next(dietArray.Length);
             return dietArray[i];
          }
     }
--- Homunculus Generator/source/People.cs
+++ Homunculus Generator/source/People.cs
@@ -4,11 +4,10 @@ namespace PeopleLibrary
 {
     public class People
     {
-        readonly private static Random randomNum = new Random();
         public static String GetPerson() 
         {
             string[] nameArray = { "Mokiro Kuzen", "Laced Azalea", "Nia Kohana", "Isabella Esperia", "Ophelia Vexx", "Jessie Zaeora", "Rosaria Shinozaki", "Sai Hamori", "Macaroni Bowl", "Doctor Dubu", "Rustie Yiffany", "Danny Naoko", "Cheap Shot", "Tea Oolong", "Flash Reaper", "Kait Oreal", "Nellie Antoinette", "Seraph Bell", "Zen Rancor", "Myras Gunhilda", "Slumpy Wing" };
-            var i = randomNum.Next(0, nameArray.Length);
+            var i = new Random().Next(nameArray.Length);
             return nameArray[i];
         }
     }
